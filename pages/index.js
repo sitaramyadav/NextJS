7574d89698062f1d1730aa36388fd3ed918ead5b
Seetaram Yadav <in-seetaram.yadav@Seetarams-MacBook-Pro-2.local>
2020-02-07 13:49:23 +0530

    Added dynamic route.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
 import Layout from "../components/Layout";
+import Link from "next/link";
 
-export default function Index() {
+const PostLink = ({ id }) => (
+  <li>
+    <Link href="/p/[id]" as={`/p/${id}`}>
+      <a>{id}</a>
+    </Link>
+  </li>
+);
+
+export default function Blog() {
   return (
     <Layout>
-      <p>Hello Next.js</p>
+      <h1>My Blogs</h1>
+      <ul>
+        <PostLink id="Stress" />
+        <PostLink id="Angularjs two way binding" />
+      </ul>
     </Layout>
   );
 }
